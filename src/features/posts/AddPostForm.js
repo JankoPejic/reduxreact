236ead12a881f59b44e7ec67e0fc51954fb5583f
JankoPejic <janko_janko@live.com>
2024-03-02 21:02:33 +0100

    chapter 3

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
 
 import { postAdded } from './postsSlice';
+import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
   const dispatch = useDispatch();
@@ -11,17 +11,27 @@ const AddPostForm = () => {
   const [content, setContent] = useState('');
   const [userId, setUserId] = useState('');
 
+  const users = useSelector(selectAllUsers);
+
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle('');
       setContent('');
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -37,7 +47,8 @@ const AddPostForm = () => {
         />
         <label htmlFor="postAuthor">Author:</label>
         <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-          <option value=""></option>
+          <option value=""></option> // Add an empty option to the select
+          {usersOptions}
         </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
@@ -46,7 +57,7 @@ const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
