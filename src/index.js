14349cb2d88468391538d94ea3fec4c9a7677524
JankoPejic <janko_janko@live.com>
2024-03-15 21:34:16 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Dispatch the action to fetch users
 store.dispatch(fetchUsers());
 
-ReactDOM.render(
+// Use createRoot instead of ReactDOM.render
+const root = createRoot(document.getElementById('root'));
+
+// Render your app within the root
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
